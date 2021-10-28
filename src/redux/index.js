a3e12aa5ce725d1f3ad2d0f977fab3722b90ff94
Subhankar Maiti <subhankarmaiti40@gmail.com>
2021-10-28 11:44:24 +0530

    added redux-devtools-extension with react-native-config to run in only development env

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 
+import Config from 'react-native-config';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducers';
 import thunk from 'redux-thunk';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const middleware = applyMiddleware(thunk);
+export default createStore(
+  rootReducer,
+  Config.ENVIRONMENT === 'development'
+    ? composeWithDevTools(middleware)
+    : middleware,
+);
