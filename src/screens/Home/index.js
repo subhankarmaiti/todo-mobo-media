89c03b9d1affe566a51a6fe9cb578db99888ede9
Subhankar Maiti <subhankarmaiti40@gmail.com>
2021-10-28 19:32:15 +0530

    integrated home screen with component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,27 +1,56 @@
-import { FlatList, SafeAreaView } from 'react-native';
+import { BackHandler, FlatList, SafeAreaView } from 'react-native';
 import React, { useEffect } from 'react';
+import { getTodoBasedOnPage, isMoreDataPresent } from 'utils/todoHelper';
+import { getTodos, nextPage, previousPage } from 'store/actions/TodoAction';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Header from 'patterns/atoms/Header';
 import Loader from 'patterns/atoms/Loader';
 import Styles from './Home.styles';
 import TodoCard from 'patterns/molecules/TodoCard';
-import { getTodos } from 'store/actions/TodoAction';
 import { useTheme } from 'native-base';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { loading, list } = useSelector(state => state.todo);
-  console.log(list, 'loading');
+  const { loading, list, page } = useSelector(state => state.todo);
+  const todos = Object.values(list);
   const { colors } = useTheme();
   const styles = Styles(colors);
-  console.log(colors);
+
   useEffect(() => {
     dispatch(getTodos());
   }, []);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (page > 1) {
+        onBack();
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, [page]);
+
+  const onNext = () => dispatch(nextPage());
+  const onBack = () => dispatch(previousPage());
   return (
     <SafeAreaView style={styles.container}>
+      <Header
+        title={page > 1 ? `Todo List ${page}` : 'Todo'}
+        enableNext={isMoreDataPresent(todos, page)}
+        onPressNext={onNext}
+        enableBack={page > 1}
+        onPressBack={onBack}
+      />
       <FlatList
-        data={Object.values(list)}
+        data={getTodoBasedOnPage(todos, page)}
         keyExtractor={item => item.id}
         renderItem={props => <TodoCard {...props} />}
         numColumns={2}
